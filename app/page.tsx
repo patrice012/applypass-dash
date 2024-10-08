9d2af7c17226b38e6f097c633de4589c2eb5f0de
yao0237 <yao874532@gmail.com>
2024-10-08 16:35:58 +0000

    feat : add job detail pop up

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { JobDetail } from "@/components/dialogs/job-detail";
 import { useEffect, useState } from "react";
 import { useWindowSize } from "@/helpers/utils";
 
@@ -76,8 +77,7 @@ const Dashboard = () => {
           <div
             className={`${
               isOpened ? "lg:ml-[250px]" : ""
-            } flex grow flex-col h-screen w-full `}
-          >
+            } flex grow flex-col h-screen w-full `}>
             <Header
               handleOpenDrawer={() => setIsOpened(!isOpened)}
               isDrawerOpen={isOpened}
@@ -126,8 +126,7 @@ const Dashboard = () => {
                         <Button
                           variant="outline"
                           size="sm"
-                          className="text-sm rounded-full h-[38px] py-[12px] px-[20px] gap-[20px]"
-                        >
+                          className="text-sm rounded-full h-[38px] py-[12px] px-[20px] gap-[20px]">
                           <div className="flex gap-2 items-center">
                             <ListFilter className="h-3.5 w-3.5" />
                             <span className="text-nowrap text-[14px]">
@@ -139,8 +138,7 @@ const Dashboard = () => {
                       </DropdownMenuTrigger>
                       <DropdownMenuContent
                         align="end"
-                        className="bg-[#F3F4F6] p-[8px]"
-                      >
+                        className="bg-[#F3F4F6] p-[8px]">
                         <DropdownMenuItem className="gap-[12px] p-[12px] cursor-pointer bg-[#fff] hover:rounded-[4px]">
                           <span className="text-[#231232] text-[14px]">
                             Match Score:
@@ -172,8 +170,7 @@ const Dashboard = () => {
                 <div className="w-full sm:justify-end flex md:hidden mt-6  sm:items-center gap-2">
                   <DropdownMenu
                     open={open}
-                    onOpenChange={(open) => setOpen(open)}
-                  >
+                    onOpenChange={(open) => setOpen(open)}>
                     <DropdownMenuTrigger
                       {...(isTouchDevice
                         ? {
@@ -182,13 +179,11 @@ const Dashboard = () => {
                           }
                         : undefined)}
                       className="w-full sm:max-w-max"
-                      asChild
-                    >
+                      asChild>
                       <Button
                         variant="outline"
                         size="sm"
-                        className="text-sm flex justify-between rounded-full h-[48px] sm:h-[38px] py-[12px] px-[20px] gap-[20px]"
-                      >
+                        className="text-sm flex justify-between rounded-full h-[48px] sm:h-[38px] py-[12px] px-[20px] gap-[20px]">
                         <div className="flex gap-2 items-center">
                           <ListFilter className="h-3.5 w-3.5" />
                           <span className=" text-[14px]">Filter by:</span>
@@ -198,8 +193,7 @@ const Dashboard = () => {
                     </DropdownMenuTrigger>
                     <DropdownMenuContent
                       align="end"
-                      className="bg-[#F3F4F6]  w-full sm:max-w-max p-[8px]"
-                    >
+                      className="bg-[#F3F4F6]  w-full sm:max-w-max p-[8px]">
                       <DropdownMenuItem className="gap-[12px] p-[12px] cursor-pointer bg-[#fff] hover:rounded-[4px]">
                         <span className="text-[#231232] text-[14px]">
                           Match Score:
@@ -238,49 +232,55 @@ const Dashboard = () => {
                   <div className="flex flex-col gap-[15px] mt-6">
                     {Array.from({ length: 3 }).map((_e, idx: number) => {
                       return (
-                        <div key={idx} className="flex w-full items-center ">
-                          <div className="w-full flex flex-col md:flex-row gap-4  md:items-center py-4 px-[10px] md:p-4 rounded-xl bg-[#F6F5F4] border border-[#E5E7EB] justify-between">
-                            <div className="flex w-full gap-[12px] items-start">
-                              <div className="size-14 border-[1.5px] border-[#E7E9EB] rounded-full bg-white p-2">
-                                <img
-                                  src={
-                                    "https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
-                                  }
-                                  alt=""
-                                />
-                              </div>
-                              <div className="flex flex-col gap-[6px]">
-                                <span className="text-[#231232] font-semibold text-[14px] md:text-[16px]">
-                                  Site Reliability Engineer (SRE)
-                                </span>
-                                <span className="text-[#231232] text-[12px] md:text-[14px]">
-                                  Manager | On Site | VueJS | Google LLC
-                                </span>
-                                <span className="text-[#231232] text-[12px] md:text-[14px]">
-                                  Match Score:{" "}
-                                  <span className="text-[#231232] text-[12px] md:text-[14px] font-bold">
-                                    99%
+                        <JobDetail>
+                          <div key={idx} className="flex w-full items-center ">
+                            <div className="w-full flex flex-col md:flex-row gap-4  md:items-center py-4 px-[10px] md:p-4 rounded-xl bg-[#F6F5F4] border border-[#E5E7EB] justify-between">
+                              <div className="flex w-full gap-[12px] items-start">
+                                <div className="size-14 border-[1.5px] border-[#E7E9EB] rounded-full bg-white p-2">
+                                  <img
+                                    src={
+                                      "https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
+                                    }
+                                    alt=""
+                                  />
+                                </div>
+                                <div className="flex flex-col gap-[6px]">
+                                  <span className="text-[#231232] font-semibold text-[14px] md:text-[16px]">
+                                    Site Reliability Engineer (SRE)
                                   </span>
-                                </span>
-                                <span className=" flex md:hidden text-[#231232] text-[12px] md:text-[14px]">
-                                  Applied on:{" "}
-                                  <span className="text-[#231232] text-[12px] md:text-[14px] font-bold">
-                                    19th August, 2024
+                                  <span className="text-[#231232] text-[12px] md:text-[14px]">
+                                    Manager | On Site | VueJS | Google LLC
                                   </span>
-                                </span>
+                                  <span className="text-[#231232] text-[12px] md:text-[14px]">
+                                    Match Score:{" "}
+                                    <span className="text-[#231232] text-[12px] md:text-[14px] font-bold">
+                                      99%
+                                    </span>
+                                  </span>
+                                  <span className=" flex md:hidden text-[#231232] text-[12px] md:text-[14px]">
+                                    Applied on:{" "}
+                                    <span className="text-[#231232] text-[12px] md:text-[14px] font-bold">
+                                      19th August, 2024
+                                    </span>
+                                  </span>
+                                </div>
                               </div>
-                            </div>
-                            <div className="flex gap-[8px]">
-                              <button className="bg-[#34D399] hover:bg-[#fff] hover:text-[#34D399] transition ease-in-out duration-500  w-full justify-center md:items-center flex gap-[8px] items-center rounded-full border border-[#059669]  px-[16px] md:w-[180px] py-[10px] text-[14px] font-medium  text-[#fff] text-nowrap">
-                                <User size={20} color="#FDE68A" /> Added to
-                                Queue
-                              </button>
-                              <button className="h-[40px] shrink-0 w-[40px] flex justify-center items-center rounded-full bg-[#fff] hover:bg-[#EF4444]/10 transition ease-in-out duration-500 border border-[#D1D5DB]">
-                                <Minus color="#EF4444" />
-                              </button>
+                              <div className="flex gap-[8px]">
+                                <button
+                                  onClick={(e) => {
+                                    e.preventDefault();
+                                  }}
+                                  className="bg-[#34D399] hover:bg-[#fff] hover:text-[#34D399] transition ease-in-out duration-500  w-full justify-center md:items-center flex gap-[8px] items-center rounded-full border border-[#059669]  px-[16px] md:w-[180px] py-[10px] text-[14px] font-medium  text-[#fff] text-nowrap">
+                                  <User size={20} color="#FDE68A" /> Added to
+                                  Queue
+                                </button>
+                                <button className="h-[40px] shrink-0 w-[40px] flex justify-center items-center rounded-full bg-[#fff] hover:bg-[#EF4444]/10 transition ease-in-out duration-500 border border-[#D1D5DB]">
+                                  <Minus color="#EF4444" />
+                                </button>
+                              </div>
                             </div>
                           </div>
-                        </div>
+                        </JobDetail>
                       );
                     })}
                     {Array.from({ length: 3 }).map((_e, idx: number) => {
---
+++ components/dialogs/job-detail.tsx
@@ -0,0 +1,95 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { PropsWithChildren } from "react";
+import { Textarea } from "../ui/textarea";
+import { Separator } from "../ui/separator";
+import { Checkbox } from "../ui/checkbox";
+
+export function JobDetail({ children }: PropsWithChildren) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="mt-[100px] h-full md:h-auto m-auto sm:max-w-[700px] max-w-[90%] bg-[#FBFAF8] overflow-scroll md:overflow-auto">
+        <DialogHeader className="flex flex-col gap-3 items-start">
+          <DialogTitle className="text-2xl font-bold">
+            Log Interview
+          </DialogTitle>
+          <Separator />
+          <div className="flex flex-col gap-2 items-start">
+            <DialogDescription className="text-lg text-[#231232] font-semibold text-start">
+              Congratulations on interview you got with ApplyPass.
+            </DialogDescription>
+            <DialogDescription className="text-start">
+              With this data, we will work to get you more matches like this in
+              the future.
+            </DialogDescription>
+          </div>
+        </DialogHeader>
+        <div className="flex flex-col gap-6 py-4">
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="name" className="text-start">
+              When did you receive the offer for this interview ?
+            </label>
+            <Input
+              placeholder="Enter number"
+              id="name"
+              className="col-span-3"
+            />
+          </div>
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="username" className="text-start">
+              What is your interview date ?
+            </label>
+            <Input
+              placeholder="Enter number"
+              id="username"
+              className="col-span-3"
+            />
+          </div>
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="username" className="text-start">
+              What excites and/or doesn&apos;t excites you about this interview ?
+            </label>
+            <Textarea
+              placeholder="Enter your message here"
+              id="username"
+              className="col-span-3"
+            />
+          </div>
+          <div className="flex gap-3">
+            <Checkbox id="terms" className="" />
+            <label
+              htmlFor="terms"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              I want to avoid applying to any other listings for this company
+            </label>
+          </div>
+        </div>
+        <DialogFooter className="flex flex-co md:grid md:grid-cols-2 gap-3">
+          <Button
+            className="bg-transparent text-[#6805DA] border border-[#6805DA] hover:bg-transparent py-6 w-full rounded-[100px]"
+            type="submit"
+          >
+            Maybe later
+          </Button>
+          <Button
+            className="py-6 w-full rounded-[100px] border border-[#6805DA] bg-[#6805DA] hover:bg-[#6805DA]/60"
+            type="submit"
+          >
+            Log Interview
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
