1a1dd2e247aac637b69d5a58e3c72cae79e0bcbd
yao0237 <yao874532@gmail.com>
2024-10-08 16:32:13 +0000

    fix: setting app layout

---
+++ components/app/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useWindowSize } from "@/helpers/utils";
+import Header from "./header";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+const AppLayout = ({ children }: PropsWithChildren) => {
+  //
+  const [width] = useWindowSize();
+  const [isOpened, setIsOpened] = useState(width > 1024);
+
+  useEffect(() => {
+    setIsOpened(width > 1024);
+  }, [width]);
+
+  return (
+    <div className="flex h-screen w-full  bg-muted/40">
+      <div
+        className={`${
+          isOpened ? "lg:ml-[250px]" : ""
+        } flex grow flex-col h-screen w-full `}
+      >
+        <Header
+          handleOpenDrawer={() => setIsOpened(!isOpened)}
+          isDrawerOpen={isOpened}
+        />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
