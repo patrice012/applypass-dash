630a6314e767685a4714913f4f62cec4505d7d75
yao0237 <yao874532@gmail.com>
2024-10-09 16:31:42 +0000

    update: reponsive setting navigation

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -6,6 +6,7 @@ import Account from "@/components/app/settings/account";
 import Security from "@/components/app/settings/security";
 import Plan from "@/components/app/settings/plan";
 import Notif from "@/components/app/settings/notif";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const SettingsPage = () => {
   const [activeSection, setActiveSection] = useState("account");
@@ -27,7 +28,32 @@ const SettingsPage = () => {
 
   return (
     <div className="main-content">
-      <span className="font-bold text-2xl">Settings</span>
+      <div className="flex flex-col gap-[16px]">
+        <span className="font-bold text-2xl">Settings</span>
+        <Tabs defaultValue={activeSection} className="flex md:hidden">
+          <TabsList className="sm:justify-between gap-[24px] justify-start w-full md:gap-[20px] md:justify-start overflow-x-auto scrollbar-hide ">
+            <TabsTrigger
+              value="account"
+              onClick={() => setActiveSection("account")}>
+              Profile
+            </TabsTrigger>
+            <TabsTrigger
+              value="security"
+              onClick={() => setActiveSection("security")}>
+              Security
+            </TabsTrigger>
+            <TabsTrigger value="plan" onClick={() => setActiveSection("plan")}>
+              Plan
+            </TabsTrigger>
+            <TabsTrigger
+              value="notifications"
+              onClick={() => setActiveSection("notifications")}>
+              Notifications
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+
       <Separator />
       <div className="flex gap-[20px] xl:gap-[60px] lg:gap-[40px] items-start">
         <div className="bg-[#fff] sticky top-[0px] p-[30px] text-[#757F87] hidden md:flex flex-col  rounded-[10px] w-full max-w-[300px] gap-[10px] border shadow-md">
@@ -37,8 +63,7 @@ const SettingsPage = () => {
                 ? "bg-[#EDF3FC] text-[#1165EF] font-semibold"
                 : " hover:text-[#1165EF] hover:font-semibold"
             }`}
-            onClick={() => setActiveSection("account")}
-          >
+            onClick={() => setActiveSection("account")}>
             <User size={18} className="text-inherit" />
             <span>Profile</span>
           </div>
@@ -48,8 +73,7 @@ const SettingsPage = () => {
                 ? "bg-[#EDF3FC] text-[#1165EF] font-semibold"
                 : " hover:text-[#1165EF] hover:font-semibold"
             }`}
-            onClick={() => setActiveSection("security")}
-          >
+            onClick={() => setActiveSection("security")}>
             <Key size={18} className="text-inherit" />
             <span>Login and Security</span>
           </div>
@@ -59,8 +83,7 @@ const SettingsPage = () => {
                 ? "bg-[#EDF3FC] text-[#1165EF] font-semibold"
                 : " hover:text-[#1165EF] hover:font-semibold"
             }`}
-            onClick={() => setActiveSection("plan")}
-          >
+            onClick={() => setActiveSection("plan")}>
             <CreditCard size={18} className="text-inherit" />
             <span>Plan</span>
           </div>
@@ -70,8 +93,7 @@ const SettingsPage = () => {
                 ? "bg-[#EDF3FC] text-[#1165EF] font-semibold"
                 : " hover:text-[#1165EF] hover:font-semibold"
             }`}
-            onClick={() => setActiveSection("notifications")}
-          >
+            onClick={() => setActiveSection("notifications")}>
             <Bell size={18} className="text-inherit" />
             <span>Notifications</span>
           </div>
